583d4775b8dcc43cf5fe98f305ffed8aa1c1c674
sling06 <pkwaw@naver.com>
2022-11-30 16:21:31 +0900

    video 재생 수정

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,33 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { videos } from "../../constants";
 import { images } from "../../constants";
 import "./Landing.scss";
 
 const Landing = () => {
+  const [videoOn, setVideoOn] = useState(0);
   const navigate = useNavigate();
   const handleClick = () => {
     navigate("/phase");
   };
 
+  const videoCheck = () => {
+    setVideoOn(1);
+  };
 
   return (
     <div className="landing_container">
-      <video
-        className="landing_video"
-        autoPlay
-        muted
-        loop
-        src={videos.LandingVideo}
-        type="video/mp4"
-      />
+      {videoOn === 0 ? (
+        <>
+          <video
+            className="landing_video"
+            src={videos.LandingVideo}
+            autoPlay
+            muted
+            loop
+            type="video/mp4"
+          />
 
-      <img
-        className="landing_icon_helmet"
-        src={images.LandingHelmetImg}
-        alt="helmet_image"
-        onClick={handleClick}
-      />
+          <img
+            className="landing_icon_helmet"
+            src={images.LandingHelmetImg}
+            alt="helmet_image"
+            onClick={handleClick}
+          />
+        </>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
